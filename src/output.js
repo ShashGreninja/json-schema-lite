bb3773aaf4ce621e2ddd6c84360416065a1d8594
Shash <163015341+ShashGreninja@users.noreply.github.com>
2025-04-06 13:07:46 +0530

    New fields added to constructor in output.js

--- src/output.js
+++ src/output.js
@@ -10,18 +10,22 @@ export class Output {
   errors;
 
   /**
-   * @param {boolean} valid
-   * @param {JsonNode} keywordNode
-   * @param {JsonNode} instanceNode
-   * @param {Output[]} [errors]
-   */
-  constructor(valid, keywordNode, instanceNode, errors) {
+ * @param {boolean} valid
+ * @param {JsonNode} keywordNode
+ * @param {JsonNode} instanceNode
+ * @param {string} keyword
+ * @param {Output[]} [errors]
+ */
+  constructor(valid, keywordNode, instanceNode, keyword, errors) {
     this.valid = valid;
-    this.absoluteKeywordLocation = keywordNode.location;
+    this.keywordLocation = keywordNode.location;    //location of the keyword relative to the schema node being evaluated
+    this.absoluteKeywordLocation = keywordNode.location;    //absolute location of the keyword in the schema
     this.instanceLocation = instanceNode.location;
+    this.keyword = keyword;
+    this.error = `current instance should pass "${keyword}" keyword validation`;
 
     if (errors) {
       this.errors = errors;
     }
   }
-}
+};
